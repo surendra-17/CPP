4ca5915e4be1260ab5e4fa9b3a2ffd7b662941d2
Surendra Sawant <sawantsurendra96@gmail.com>
2024-09-22 20:48:54 +0530

    linear search find  all occurances using vector

--- DSA/linear_search.cpp
+++ DSA/linear_search.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int linearSearch(int array[],int target){
+int linearSearch(vector<int> arr,int target){
+    int size = arr.size();
+    cout << "size:" << size << endl;
+    //handle edge cases
+    if(size == 0) return -1;
+    if(arr.back() == target) return size-1;
 
-    for (int i = 0; i < sizeof(array[0]); i++)
+    for (int i = 0; i < size; i++)
     {
-        if(array[i] == target){
+        if(arr[i] == target){
             return i;
         }
     }
@@ -13,8 +19,43 @@ int linearSearch(int array[],int target){
     
 }
 
+vector<int> linearSearchWithAllOccurances(vector<int> vec,int target){
+     int size = vec.size();
+    cout << "vec size:" << size << endl;
+    vector<int> finalVec;
+    for (int i = 0; i < vec.size(); i++)
+    {
+        if(vec[i] == target){
+            finalVec.push_back(i);
+        }
+    }
+    if(finalVec.empty()){
+        finalVec.push_back(-1);
+    }
+    cout << "finalvec:" << finalVec.size()<<endl;
+    return finalVec;
+
+}
+
 int main(){
-    int array[] = {1,2,3,4,5};
+    //linear search with single element
+    vector<int> vec = {1,2,3,4,5};
+    vector<int> emptyArray ={};
     int target = 4;
-    cout << "Element Found At Index: " << linearSearch(array,target);
+    cout << "Element Found At Index: " << linearSearch(vec,target) << endl;
+    cout << "Empty Array: " << linearSearch(emptyArray,target);
+
+    cout << endl;
+    //linear serach with all ocurance of target using vector
+    vector<int> allOccuranceVec = {1,2,3,4,5,3};
+    int targetInVec = 3;
+    vector<int> result = linearSearchWithAllOccurances(allOccuranceVec,targetInVec);
+    for (int i : result)
+    {
+        cout << "Element "<< targetInVec << " Found At Index: " << i << endl;
+        
+    }
+
+    return 0;
+    
 }
