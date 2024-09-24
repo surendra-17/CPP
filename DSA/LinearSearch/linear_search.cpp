ff931318299c901ae6e21d12a2552e6df8cd309e
Surendra Sawant <sawantsurendra96@gmail.com>
2024-09-24 16:16:48 +0530

    program to  find last occurance in array and added markdow for linear search

--- DSA/LinearSearch/linear_search.cpp
+++ DSA/LinearSearch/linear_search.cpp
@@ -19,9 +19,8 @@ int linearSearch(vector<int> arr,int target){
     
 }
 
-vector<int> linearSearchWithAllOccurances(vector<int> vec,int target){
-     int size = vec.size();
-    cout << "vec size:" << size << endl;
+vector<int> linearSearchWithAllOccurrences(vector<int> vec,int target){
+    int size = vec.size();
     vector<int> finalVec;
     for (int i = 0; i < vec.size(); i++)
     {
@@ -37,6 +36,17 @@ vector<int> linearSearchWithAllOccurances(vector<int> vec,int target){
 
 }
 
+int findLastOccurrence(vector<int> vec,int target){
+    int lastOccurance = -1;
+    for (int i = 0; i < vec.size(); i++)
+    {
+        if(vec[i] == target){
+           lastOccurance = i;
+        }
+    }
+    return lastOccurance;
+}
+
 int main(){
     //linear search with single element
     vector<int> vec = {1,2,3,4,5};
@@ -49,11 +59,23 @@ int main(){
     //linear serach with all ocurance of target using vector
     vector<int> allOccuranceVec = {1,2,3,4,5,3};
     int targetInVec = 3;
-    vector<int> result = linearSearchWithAllOccurances(allOccuranceVec,targetInVec);
+    vector<int> result = linearSearchWithAllOccurrences(allOccuranceVec,targetInVec);
     for (int i : result)
     {
         cout << "Element "<< targetInVec << " Found At Index: " << i << endl;
         
+    }
+
+    //find last occurance
+    vector<int> lastOccurrenceArr = {1, 2, 3, 4, 2, 5, 2};
+    int targetLastOccurrence = 2;
+
+    int index = findLastOccurrence(lastOccurrenceArr, targetLastOccurrence);
+    
+    if (index != -1) {
+        std::cout << "Last occurrence of " << targetLastOccurrence << " is at index: " << index << std::endl;
+    } else {
+        std::cout << targetLastOccurrence << " not found in the array." << std::endl;
     }
 
     return 0;
---
+++ DSA/LinearSearch/notes.md
@@ -0,0 +1,16 @@
+# Linear Search
+
+>  ***Linear search*** is a simple algorithm used to find the position of a target value within a list or array. It checks each element in sequence until the target is found or the list ends.
+
+#### <u>Characteristics of Linear Search</u>
+
+* Time Complexity: O(n) — in the worst case, you may need to check every element.
+
+* Space Complexity: O(1) — it uses a constant amount of extra space.
+
+* Best For: Small arrays or unsorted data, where other search algorithms may not be efficient.
+
+
+
+
+
